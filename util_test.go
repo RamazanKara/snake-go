9925b6affe976347af4ca5a7ef727b4319bcc57b
RamazanKara <mail@r-kara.de>
2023-12-23 05:28:32 +0100

    chore: add test for utils

---
+++ util_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "image/color"
+    "testing"
+)
+
+func TestRainbowColor(t *testing.T) {
+    // Test for a few different frame counts
+    frameCounts := []int{0, 10, 20, 30}
+    for _, frameCount := range frameCounts {
+        got := rainbowColor(frameCount)
+        _, _, _, a := got.RGBA()
+        if a != 0xffff {
+            t.Errorf("rainbowColor(%v) = %v; want alpha value 0xffff", frameCount, got)
+        }
+    }
+}
